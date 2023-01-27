331fa06c093ea8d2e88a356673ddf216a35e4191
unknown <tarunkchandra@gmail.com>
2023-01-27 11:56:10 +0530

    contact-list

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
+var contactList = [
+  {
+    name: "Rio",
+    phone: "1111111111",
+  },
+  {
+    name: "Tony Stark",
+    phone: "1234567890",
+  },
+  {
+    name: "Richard",
+    phone: "12131321321",
+  },
+];
+const path = require("path");
+app.set("view engine", "ejs");
+app.set("Views", path.join(__dirname, "Views"));
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("index", {
+    title: "node class",
+  });
 });
-
+app.get("/viewlist", (req, res) => {
+  res.render("index", {
+    title: "contact",
+    list: {
+      name: contactList,
+    },
+  });
+});
+// app.use(path.join(__dirname, "public"));
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
