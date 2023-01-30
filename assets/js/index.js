62b7bb76d69548a63a909c712b8ef0a8d061634d
unknown <tarunkchandra@gmail.com>
2023-01-30 11:43:13 +0530

    view11

---
+++ assets/js/index.js
@@ -0,0 +1,1 @@
+console.log("we do not have any issue");
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+
 var contactList = [
   {
     name: "Rio",
@@ -18,18 +19,38 @@ var contactList = [
 const path = require("path");
 app.set("view engine", "ejs");
 app.set("Views", path.join(__dirname, "Views"));
+
+app.use(express.urlencoded());
+app.use(express.static("assets"));
 app.get("/", (req, res) => {
   res.render("index", {
     title: "node class",
+    user: {
+      name: "ra,aN",
+    },
+    list: {
+      name: contactList,
+    },
   });
 });
 app.get("/viewlist", (req, res) => {
-  res.render("index", {
+  res.render("project", {
     title: "contact",
     list: {
       name: contactList,
     },
   });
+});
+app.post("/contact", (req, res) => {
+  console.log("expected error:", req.body);
+  contactList.push(req.body);
+  return res.redirect("/viewlist");
+  // res.render("project", {
+  //   title: "contact",
+  //   list:
+  //     name: contactList,
+  //   },
+  // });
 });
 // app.use(path.join(__dirname, "public"));
 app.listen(port, () => {
